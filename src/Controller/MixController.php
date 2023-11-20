8ca4a4d9d64df89f36dcde2e0fdbde0b69c052a2
Rahim_Bn <rahimbennejma50@gmail.com>
2023-11-20 16:01:11 +0100

    Querying the Database

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -2,32 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\VinylMix;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MixController extends AbstractController
 {
-    #[Route('/mix/new')]
-    public function new(EntityManagerInterface $entityManager): Response
+    #[Route('/mix', name: 'app_mix')]
+    public function index(): Response
     {
-        $mix = new VinylMix();
-        $mix->setTitle('Do you Remember... Phil Collins?!');
-        $mix->setDescription('A pure mix of drummers turned singers!');
-        $genre = ['pop', 'rock'];
-        $mix->setGenre($genre[array_rand($genre)]);
-        $mix->setTrackCount(rand(5, 20));
-        $mix->setVotes(rand(-50, 50));
-
-        $entityManager->persist($mix);
-        $entityManager->flush();
-
-        return new Response(sprintf(
-            'Mix %d is %d tracks of pure 80\'s heaven',
-            $mix->getId(),
-            $mix->getTrackCount()
-        ));
+        return $this->render('mix/index.html.twig', [
+            'controller_name' => 'MixController',
+        ]);
     }
 }
