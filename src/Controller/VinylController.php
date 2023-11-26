f57951bded4d60035fd91c522ca41b8a540a19d1
Rahim_Bn <rahimbennejma50@gmail.com>
2023-11-26 21:31:01 +0100

    "Custom Entity Methods & Twig Magic"

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,13 +3,20 @@
 namespace App\Controller;
 
 use App\Repository\VinylMixRepository;
-use function Symfony\Component\String\u;
+use App\Service\MixRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
+    public function __construct(
+        private bool $isDebug,
+        private MixRepository $mixRepository
+    )
+    {}
+
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
@@ -21,6 +28,7 @@ class VinylController extends AbstractController
             ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
+
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
@@ -31,9 +39,12 @@ class VinylController extends AbstractController
     public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
         $mixes = $mixRepository->findAll();
+
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
             'mixes' => $mixes,
         ]);
-    }}
+    }
+}
